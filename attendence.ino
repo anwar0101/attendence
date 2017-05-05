260de05eb3faffdaa2458f6f44c7841ce1ccb4e0
Anwar Hussen <anwar.hussen4@gmail.com>
2017-05-05 21:59:44 +0600

    Employee

---
+++ attendence.ino
@@ -0,0 +1,55 @@
+/*
+ * Initial Author: ryand1011 (https://github.com/ryand1011)
+ *
+ * Reads data written by a program such as "rfid_write_personal_data.ino"
+ *
+ * See: https://github.com/miguelbalboa/rfid/tree/master/examples/rfid_write_personal_data
+ *
+ * Uses MIFARE RFID card using RFID-RC522 reader
+ * Uses MFRC522 - Library
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+*/
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+String id1="";
+
+//*****************************************************************************************//
+void setup() {
+  Serial.begin(9600);                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+}
+
+//*****************************************************************************************//
+void loop() {
+  id1="";
+  if(mfrc522.PICC_IsNewCardPresent()&&mfrc522.PICC_ReadCardSerial()){
+      for(byte i=0; i<mfrc522.uid.size; i++) {
+          unsigned int id = mfrc522.uid.uidByte[i];
+          id1 =id1 + id;
+      }
+      
+      Serial.print("s");    //to print id of Tag in serial monitor
+      Serial.print(id1);    //to print id of Tag in serial monitor
+      Serial.print("e");    //to print id of Tag in serial monitor
+      mfrc522.PICC_HaltA();
+      Serial.print("\n");   //to give newline to seril output
+  }
+  
+}
+//*****************************************************************************************//
